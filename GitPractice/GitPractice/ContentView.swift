c257701ff328d8a132f6aa1688ca6d5a81401345
Rin Hiejima <rinhiejima@MacBook-Pro.local>
2020-07-08 11:48:44 +0900

    Add 'Metre to Feet Slider'

--- GitPractice/GitPractice/ContentView.swift
+++ GitPractice/GitPractice/ContentView.swift
@@ -9,8 +9,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var metre: Double = 0
+    @State var isChanged = false
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("Hello, World!")
+            Slider(value: $metre, in: -100...100, step: 0.1, onEditingChanged:  {
+                changed in
+                self.isChanged = changed
+            }, minimumValueLabel: Text("-100"),
+               maximumValueLabel: Text("100"),
+               label: { EmptyView() })
+            
+            if !isChanged {
+                Text("\(metre) m is  \(metre * 0.3048) feet")
+            } else {
+                Text("\(metre)")
+            }
+        }
     }
 }
 
